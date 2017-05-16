c3175f26eec227848db6dff87381fe55f9f9454d
sanchit3065 <sanchit.sncht@gmail.com>
2017-05-16 16:58:23 +0530

    Create addition and multiplication of three numbers.c

---
+++ addition and multiplication of three numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  printf("Enter three numbers");
+  int a,b,d;
+  scanf("%d %d %d",&a,&b,&c);
+  int c=a*b*d;
+  int e=a+b+d;
+  printf("Multiplication of three numbers:%d",c);
+  printf("Addition of three numbers:%d",e);
+  
+}
